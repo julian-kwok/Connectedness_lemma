1bccf5972c1b4ef647d0ffdbc3e304eb7ad7ab1f
Tsz Hei Kwok <julian.kwok0114@gmail.com>
2026-05-25 18:31:53 +0800

    proof is finished

--- ConnectednessLemma/Basic.lean
+++ ConnectednessLemma/Basic.lean
@@ -10,129 +10,128 @@ variable {n : ℕ} (A : Matrix (Fin n) (Fin n) ℝ)
 
 /- Define skew-symmetric matrix -/
 def IsSkewSymm' {n : ℕ} (A : Matrix (Fin n) (Fin n) ℝ) : Prop :=
-  ∀ i j : Fin n, A i j = - A j i
+    ∀ i j : Fin n, A i j = - A j i
 
 
 /- Define connectedness of vertices -/
 def IsConnTo (i j : (Fin n)) : Prop :=
-  -- Here I add explicitly that A is SkewSymm
-  (IsSkewSymm' A) ∧ (A i j ≠ 0) ∧ (∀ k, k ≠ i ∧ k ≠ j → ∃ m : ℕ, (A i k + A k j) / (A j i) = ↑m)
+    (IsSkewSymm' A) ∧ (A i j ≠ 0) ∧ (∀ k, k ≠ i ∧ k ≠ j → ∃ m : ℕ, (A i k + A k j) / (A j i) = ↑m)
 
 
 lemma Symmetry (i j : Fin n) (h1 : IsConnTo A i j) : IsConnTo A j i := by
-    -- Unpack the connectedness condition
-    rcases h1 with ⟨hA, h1_1, h1_2⟩
-    -- Show that A j i is nonzero
-    have hh1 : A j i ≠ 0 := by
+   -- Unpack the connectedness condition
+   rcases h1 with ⟨hA, h1_1, h1_2⟩
+   -- Show that A j i is nonzero
+   have hh1 : A j i ≠ 0 := by
         have h2 : A i j = - A j i := hA i j
         rw [h2] at h1_1
         exact neg_ne_zero.mp h1_1
-    -- Show that the second condition holds for j and i
-    have hh2 : ∀ k, k ≠ j ∧ k ≠ i → ∃ m : ℕ, (A j k + A k i) / (A i j) = ↑m := by
+   -- Show that the second condition holds for j and i
+   have hh2 : ∀ k, k ≠ j ∧ k ≠ i → ∃ m : ℕ, (A j k + A k i) / (A i j) = ↑m := by
         intro k hk
         have h3 : k ≠ i ∧ k ≠ j := by
-            rw [and_comm] at hk
-            exact hk
+           rw [and_comm] at hk
+           exact hk
         have h4 : ∃ m : ℕ, (A i k + A k j) / (A j i) = ↑m := h1_2 k h3
         have h5 : (A j k + A k i) / (A i j) = (A i k + A k j) / (A j i) := by
-            have h6 : A j k = - A k j := hA j k
-            have h7 : A k i = - A i k := hA k i
-            rw [h6, h7, hA i j]
-            ring
+           have h6 : A j k = - A k j := hA j k
+           have h7 : A k i = - A i k := hA k i
+           rw [h6, h7, hA i j]
+           ring
         rw [← h5] at h4
         exact h4
-    exact ⟨hA, hh1, hh2⟩
+   exact ⟨hA, hh1, hh2⟩
 
 
 lemma NoLoops (i j : (Fin n)) {h : IsSkewSymm' A} : IsConnTo A i j → ¬ i = j := by
-    intro hcon hij
-    have h1 : A i i ≠ 0 := by
-        simpa [hij] using hcon.right.left
-    have h2 : A i i = 0 := by
-        have h3 : A i i = -A i i := h i i
-        linarith
-    exact h1 h2
+   intro hcon hij
+   have h1 : A i i ≠ 0 := by
+       simpa [hij] using hcon.right.left
+   have h2 : A i i = 0 := by
+       have h3 : A i i = -A i i := h i i
+       linarith
+   exact h1 h2
 
 
 lemma DiagEntrysAreZero (i : Fin n) (h : IsSkewSymm' A) : A i i = 0 := by
-    have h1 : A i i = -A i i := h i i
-    linarith
+   have h1 : A i i = -A i i := h i i
+   linarith
 
 
 /- State the Lemma statement -/
 /- If i->j->k in A, then Aij is a positive mulitple of Ajk -/
 theorem AdjEdgesPosRatio (i j k : Fin n) (h1 : IsConnTo A i j) (h2 : IsConnTo A j k) (hh : k ≠ i) :
-    ∃ m > 0, A i j = m * A j k := by
-    -- Unpack the connectedness conditions
-    rcases h1 with ⟨hA, h1_1, h1_2⟩
-    rcases h2 with ⟨hA, h2_1, h2_2⟩
-    -- A j i ≠ 0 from skew symmetry and h1_1
-    have hAji : A j i ≠ 0 := by
-        have : A i j = - A j i := hA i j
-        rw [this] at h1_1
-        exact neg_ne_zero.mp h1_1
-    -- A k j ≠ 0 from skew symmetry and h2_1
-    have hAkj : A k j ≠ 0 := by
-        have : A j k = - A k j := hA j k
-        rw [this] at h2_1
-        exact neg_ne_zero.mp h2_1
-    -- Since i->j, we have
-    have h3 : ∃ M : ℕ, A i k + A k j = M * A j i := by
-        let h1_2_k := h1_2 k
-        have hk : k ≠ i ∧ k ≠ j := by
-            have hkj : k ≠ j := by
-                intro hkj
-                rw [hkj] at h2_1
-                exact h2_1 (DiagEntrysAreZero A j hA)
-            exact ⟨hh, hkj⟩
-        let h3 := h1_2_k hk
-        obtain ⟨M, h3⟩ := h3
-        use M
-        rw [← h3]
-        let h4 := div_mul_cancel₀ (A i k + A k j) hAji
-        rw [h4]
-    -- Since j->i, we have
-    have h4 : ∃ N : ℕ, A j i + A i k = N * A k j := by
-        let h2_2_i := h2_2 i
-        have hi : i ≠ j ∧ i ≠ k := by
-            have hij : i ≠ j := by
-                intro hij
-                rw [hij] at h1_1
-                exact h1_1 (DiagEntrysAreZero A j hA)
-            have hh' : i ≠ k := by exact ne_comm.1 hh
-            exact ⟨hij, hh'⟩
-        let h4 := h2_2_i hi
-        obtain ⟨N, h4⟩ := h4
-        use N
-        rw [← h4]
-        let h5 := div_mul_cancel₀ (A j i + A i k) hAkj
-        rw [h5]
-    -- Combine the two equations
-    have h5 : ∃ M N : ℕ, (M + 1) * A i j = (N + 1) * A j k := by
-        obtain ⟨M, h3⟩ := h3
-        obtain ⟨N, h4⟩ := h4
-        use M, N
-        rw [hA i j, hA j k]
-        rw [add_mul, add_mul, one_mul, one_mul, mul_neg, mul_neg, ← h3, ← h4]
-        ring
-    -- Move terms around
-    have h6 : ∃ M N : ℕ, A i j = (N + 1) / (M + 1) * A j k := by
-        obtain ⟨M, N, h5⟩ := h5
-        use M, N
-        have hM : (M + 1) ≠ 0 := by
-            linarith
-        rw [@div_mul_eq_mul_div₀, ←h5, mul_comm, mul_div_cancel_right₀]
-        linarith
-    -- Show that the ratio is positive
-    have h7 : ∃ m > 0, A i j = m * A j k := by
+   ∃ m > 0, A i j = m * A j k := by
+   -- Unpack the connectedness conditions
+   rcases h1 with ⟨hA, h1_1, h1_2⟩
+   rcases h2 with ⟨hA, h2_1, h2_2⟩
+   -- A j i ≠ 0 from skew symmetry and h1_1
+   have hAji : A j i ≠ 0 := by
+       have : A i j = - A j i := hA i j
+       rw [this] at h1_1
+       exact neg_ne_zero.mp h1_1
+   -- A k j ≠ 0 from skew symmetry and h2_1
+   have hAkj : A k j ≠ 0 := by
+       have : A j k = - A k j := hA j k
+       rw [this] at h2_1
+       exact neg_ne_zero.mp h2_1
+   -- Since i->j, we have
+   have h3 : ∃ M : ℕ, A i k + A k j = M * A j i := by
+       let h1_2_k := h1_2 k
+       have hk : k ≠ i ∧ k ≠ j := by
+           have hkj : k ≠ j := by
+               intro hkj
+               rw [hkj] at h2_1
+               exact h2_1 (DiagEntrysAreZero A j hA)
+           exact ⟨hh, hkj⟩
+       let h3 := h1_2_k hk
+       obtain ⟨M, h3⟩ := h3
+       use M
+       rw [← h3]
+       let h4 := div_mul_cancel₀ (A i k + A k j) hAji
+       rw [h4]
+   -- Since j->i, we have
+   have h4 : ∃ N : ℕ, A j i + A i k = N * A k j := by
+       let h2_2_i := h2_2 i
+       have hi : i ≠ j ∧ i ≠ k := by
+           have hij : i ≠ j := by
+               intro hij
+               rw [hij] at h1_1
+               exact h1_1 (DiagEntrysAreZero A j hA)
+           have hh' : i ≠ k := by exact ne_comm.1 hh
+           exact ⟨hij, hh'⟩
+       let h4 := h2_2_i hi
+       obtain ⟨N, h4⟩ := h4
+       use N
+       rw [← h4]
+       let h5 := div_mul_cancel₀ (A j i + A i k) hAkj
+       rw [h5]
+   -- Combine the two equations
+   have h5 : ∃ M N : ℕ, (M + 1) * A i j = (N + 1) * A j k := by
+       obtain ⟨M, h3⟩ := h3
+       obtain ⟨N, h4⟩ := h4
+       use M, N
+       rw [hA i j, hA j k]
+       rw [add_mul, add_mul, one_mul, one_mul, mul_neg, mul_neg, ← h3, ← h4]
+       ring
+   -- Move terms around
+   have h6 : ∃ M N : ℕ, A i j = (N + 1) / (M + 1) * A j k := by
+       obtain ⟨M, N, h5⟩ := h5
+       use M, N
+       have hM : (M + 1) ≠ 0 := by
+           linarith
+       rw [@div_mul_eq_mul_div₀, ←h5, mul_comm, mul_div_cancel_right₀]
+       linarith
+   -- Show that the ratio is positive
+   have h7 : ∃ m > 0, A i j = m * A j k := by
        obtain ⟨M, N, h6⟩ := h6
        use (N + 1) / (M + 1)
        rw [h6]
        split_ands
-       · have hN1 : N + 1 > 0 := by
+       · have hN1 : N + 1 > (0 : ℝ) := by
            linarith
-         have hM1 : ↑M + 1 > 0 := by
+         have hM1 : (0 : ℝ) < (M + 1) := by
            linarith
          apply div_pos hN1 hM1
        · rfl
-    exact h7
+   exact h7
